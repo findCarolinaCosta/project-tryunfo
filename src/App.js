5841a2d4d6c19fd67dd62aa2a4685c56a4918fed
findcarolinacosta <carolinadacosta1997@gmail.com>
2021-10-13 17:38:27 -0300

    Requsito 2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
 import React from 'react';
 import Form from './components/Form';
 
+const formInfo = {
+  cardName: '',
+  cardDescription: '',
+  cardAttr1: '',
+  cardAttr2: '',
+  cardAttr3: '',
+  cardImage: '',
+  cardRare: 'Selecione',
+  cardTrunfo: false,
+  isSaveButtonDisabled: true,
+};
+
+const onInputChange = ({ target }) => {
+  const { name } = target;
+  const value = target.type === 'checkbox' ? target.checked : target.value;
+  this.setState({
+    [name]: value,
+  });
+};
+
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = formInfo;
+  }
+
   render() {
     return (
-      <div>
+      <div className="container">
         <h1>Tryunfo</h1>
-        <Form />
+        <Form { ...this.state } onInputChange={ onInputChange } />
       </div>
     );
   }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,18 +1,35 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Form extends Component {
   render() {
+    const {
+      cardName,
+      cardDescription,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardImage,
+      cardRare,
+      cardTrunfo,
+      isSaveButtonDisabled,
+      onInputChange,
+      onSaveButtonClick,
+    } = this.props;
+
     return (
       <form className="container">
 
         <div className="div-infos">
-          <label htmlFor="lettername">
+          <label htmlFor="cardName">
             Nome da carta:
             <input
               data-testid="name-input"
               type="text"
-              name="lettername"
-              id="lettername"
+              name="cardName"
+              id="cardName"
+              value={ cardName }
+              onChange={ onInputChange }
             />
           </label>
         </div>
@@ -24,6 +41,8 @@ class Form extends Component {
               data-testid="description-input"
               name="descriptionletter"
               id="descriptionletter"
+              value={ cardDescription }
+              onChange={ onInputChange }
             />
           </label>
         </div>
@@ -36,6 +55,8 @@ class Form extends Component {
               type="number"
               name="firstattribute"
               id="firstattribute"
+              value={ cardAttr1 }
+              onChange={ onInputChange }
             />
           </label>
         </div>
@@ -48,6 +69,8 @@ class Form extends Component {
               type="number"
               name="secondattribute"
               id="secondattribute"
+              value={ cardAttr2 }
+              onChange={ onInputChange }
             />
           </label>
         </div>
@@ -60,6 +83,8 @@ class Form extends Component {
               type="number"
               name="thirdattribute"
               id="thirdattribute"
+              value={ cardAttr3 }
+              onChange={ onInputChange }
             />
           </label>
         </div>
@@ -67,14 +92,28 @@ class Form extends Component {
         <div className="div-infos">
           <label htmlFor="imgcart">
             Url da imagem da carta:
-            <input data-testid="image-input" type="text" name="imgcart" id="imgcart" />
+            <input
+              data-testid="image-input"
+              type="text"
+              name="imgcart"
+              id="imgcart"
+              value={ cardImage }
+              onChange={ onInputChange }
+            />
+
           </label>
         </div>
 
         <div className="div-infos">
           <label htmlFor="rarityletter">
             Selecione a raridade da carta:
-            <select data-testid="rare-input" name="rarityletter" id="rarityletter">
+            <select
+              data-testid="rare-input"
+              name="rarityletter"
+              id="rarityletter"
+              value={ cardRare }
+              onChange={ onInputChange }
+            >
               <option>normal</option>
               <option>raro</option>
               <option>muito raro</option>
@@ -88,16 +127,41 @@ class Form extends Component {
               data-testid="trunfo-input"
               type="checkbox"
               id="super"
+              checked={ cardTrunfo }
+              onChange={ onInputChange }
             />
             É uma carta Super Trunfo? ⭐⭐⭐⭐⭐
           </label>
         </div>
 
-        <button data-testid="save-button" type="button">Salvar</button>
+        <button
+          data-testid="save-button"
+          type="button"
+          disabled={ isSaveButtonDisabled }
+          onClick={ onSaveButtonClick }
+        >
+          Salvar
+
+        </button>
 
       </form>
     );
   }
 }
+
+Form.protoTypes = {
+  cardName: PropTypes.string,
+  cardDescription: PropTypes.string,
+  cardAttr1: PropTypes.string,
+  cardAttr2: PropTypes.string,
+  cardAttr3: PropTypes.string,
+  cardImage: PropTypes.string,
+  cardRare: PropTypes.string,
+  cardTrunfo: PropTypes.bool,
+  hasTrunfo: PropTypes.bool,
+  isSaveButtonDisabled: PropTypes.bool,
+  onInputChange: PropTypes.func,
+  onSaveButtonClick: PropTypes.func,
+};
 
 export default Form;
