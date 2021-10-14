25f92eb39d810caa80e0e400dd6a0b2833a4aadc
findcarolinacosta <carolinadacosta1997@gmail.com>
2021-10-13 21:35:57 -0300

    Requisito 5

--- src/App.js
+++ src/App.js
@@ -25,6 +25,31 @@ class App extends React.Component {
     const value = target.type === 'checkbox' ? target.checked : target.value;
     this.setState({
       [name]: value,
+    }, () => {
+      const { cardName, cardDescription, cardImage, cardRare,
+        cardAttr1, cardAttr2, cardAttr3 } = this.state;
+      const limitPerAttribute = 90;
+      const sumMax = 210;
+      const sumFinalAtributes = (Number(cardAttr1)
+        + Number(cardAttr2)
+        + Number(cardAttr3)) <= sumMax;
+      const sumAtribues = Number(cardAttr1) >= 0
+        && Number(cardAttr2) >= 0
+        && Number(cardAttr3) >= 0;
+      const checkingLimitPerAttribute = Number(cardAttr1) <= limitPerAttribute
+        && Number(cardAttr2) <= limitPerAttribute
+        && Number(cardAttr3) <= limitPerAttribute;
+      if (cardName
+        && cardDescription
+        && cardImage
+        && cardRare
+        && sumAtribues
+        && checkingLimitPerAttribute
+        && sumFinalAtributes) {
+        this.setState({ isSaveButtonDisabled: false });
+      } else {
+        this.setState({ isSaveButtonDisabled: true });
+      }
     });
   };
 
